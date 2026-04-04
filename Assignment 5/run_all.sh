44f11692b1f428abfe6af17505455271a4a30ea4
Deepraj Majumdar <murieldeepraj@gmail.com>
2026-04-04 11:50:44 +0530

    Add Assignment 5.

--- Assignment 5/run_all.sh
+++ Assignment 5/run_all.sh
@@ -5,8 +5,8 @@
 # To re-run a specific step, delete its .done file from ./results/
 # ==============================================================================
 
-WANDB_KEY="XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"  # <-- CHANGE THIS to your actual Weights & Biases API key
-HF_TOKEN="hf_XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+WANDB_KEY="YOUR_ID"  # <-- CHANGE THIS to your actual Weights & Biases API key
+HF_TOKEN="YOUR_ID"
 HF_REPO="YourHFUsername/your-vit-lora-model"  # <-- CHANGE THIS to your actual HuggingFace repo name
 CPU_ONLY=true
 
